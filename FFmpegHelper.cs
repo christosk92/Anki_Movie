3ea6ab3e7658d02a2877f2c253e384c1c8f9f806
Christos <christos@marteco.nl>
2020-02-27 21:14:44 +0100

    FFMPEG helpers

---
+++ FFmpegHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
+
+namespace AnkiMovie_Console_Test
+{
+    public static class FFmpegHelper
+    {
+        public static void init(string ffmpegpath) => FFmpeg.ExecutablesPath = ffmpegpath;
+        public static async Task<string> ExtractAudio(string input, string output)
+        {
+            Console.WriteLine("Extracting audio from: " + input);
+            if (System.IO.File.Exists(output))
+                System.IO.File.Delete(output);
+            var action = Conversion.ExtractAudio(input, output);
+            action.OnProgress += (sender, args) =>
+            {
+                var percent = (int)(Math.Round(args.Duration.TotalSeconds / args.TotalLength.TotalSeconds, 2) * 100);
+                Console.WriteLine($"[{args.Duration} / {args.TotalLength}] {percent}%");
+            };
+            await action.Start();
+            Console.WriteLine("Extracted audio to: " + output);
+            return output;
+        }
+        public static async Task<string> Trim(string input, string output, TimeSpan from, TimeSpan to)
+        {
+            Console.WriteLine("Trimming audio from: " + input);
+            if (System.IO.File.Exists(output))
+                System.IO.File.Delete(output);
+            var action = Conversion.Split(output, output, from, to);
+            action.OnProgress += (sender, args) =>
+            {
+                var percent = (int)(Math.Round(args.Duration.TotalSeconds / args.TotalLength.TotalSeconds, 2) * 100);
+                Console.WriteLine($"[{args.Duration} / {args.TotalLength}] {percent}%");
+            };
+            await action.Start();
+            Console.WriteLine("Trimmed audio to: " + output);
+            return output;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Enums;
+using Xabe.FFmpeg.Model;
 
 namespace AnkiMovie_Console_Test {
 
@@ -14,55 +17,74 @@ namespace AnkiMovie_Console_Test {
         private static string videoPath;
         private static string srtPath;
         private static string AnkiMovieData;
-
-        private static async Task Main(string[] args) {
-            Console.WriteLine("Hello Weeb!");
-            string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            AnkiMovieData = Path.Combine(localAppData, "AnkiMovie");
-            Directory.CreateDirectory(AnkiMovieData);
-            if (!System.IO.File.Exists(Path.Combine(AnkiMovieData, "1.json")))
-                PickVideoAndSrt();
-            else
-                LoadVideoAndSrt();
-            Console.WriteLine("Video: " + videoPath);
-            Console.WriteLine("Srt: " + srtPath);
-            var results = GetSubtitleText(srtPath);
-            //foreach(var result in results)
-            //{
-            //  int index = results.FindIndex(x=> x.StartTime == result.StartTime);
-            //  Console.WriteLine($"{index}: from {result.StartTime} to {result.EndTime} : {result.Lines[0]}");
-            //}
-            Console.WriteLine(("").PadRight(48, '-'));
-            Console.WriteLine("Trying to poke AnkiConnect server... Give me a few..");
-            bool result = false;
-            var httpClient = new HttpClient();
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            try {
-                HttpResponseMessage response = await httpClient.GetAsync("http://127.0.0.1:8765/");
-                response.EnsureSuccessStatusCode();    // Throw if not a success code.
-                result = true;
-            }
-            catch (HttpRequestException e) {
-                // Handle exception.
-            }
-            if (result) {
-                Console.WriteLine("Anki-senpai noticed me!");
-                AnkiHelpers.init();
-                var note = new Note
+        private static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello Weeb!");
+                string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                AnkiMovieData = Path.Combine(localAppData, "AnkiMovie");
+                Directory.CreateDirectory(AnkiMovieData);
+                if (!System.IO.File.Exists(Path.Combine(AnkiMovieData, "1.json")))
+                    PickVideoAndSrt();
+                else
+                    LoadVideoAndSrt();
+                Console.WriteLine("Video: " + videoPath);
+                Console.WriteLine("Srt: " + srtPath);
+                var results = GetSubtitleText(srtPath);
+                //foreach(var result in results)
+                //{
+                //  int index = results.FindIndex(x=> x.StartTime == result.StartTime);
+                //  Console.WriteLine($"{index}: from {result.StartTime} to {result.EndTime} : {result.Lines[0]}");
+                //}
+                Console.WriteLine(("").PadRight(48, '-'));
+                Console.WriteLine("Trying to poke AnkiConnect server... Give me a few..");
+                bool result = false;
+                var httpClient = new HttpClient();
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                try
                 {
-                    DeckName = "Test Anki Movie",
-                    ModelName = "Basic",
-                    options = new Options { allowDuplicate = true},
-                    Tags = new List<String>{ "AnkiMovie" },
-                    Fields = new Fields { Back = "Test1", Front = "Test1" }
-                };
-                var resultAdd = AnkiHelpers.AddToDeck(note);
-                Console.WriteLine(resultAdd);
+                    HttpResponseMessage response = await httpClient.GetAsync("http://127.0.0.1:8765/");
+                    response.EnsureSuccessStatusCode();    // Throw if not a success code.
+                    result = true;
+                }
+                catch (HttpRequestException e)
+                {
+                    // TODO: Handle exception.
+                }
+                if (result)
+                {
+                    Console.WriteLine("Anki-senpai noticed me!");
+                    AnkiHelpers.init();
+                    //-- Works as of 2/27/2020  --
+                    /* var note = new Note
+                     {
+                         DeckName = "Test Anki Movie",
+                         ModelName = "Basic",
+                         options = new Options { allowDuplicate = true},
+                         Tags = new List<String>{ "AnkiMovie" },
+                         Fields = new Fields { Back = "Test1", Front = "Test1" }
+                     };
+                     var resultAdd = AnkiHelpers.AddToDeck(note);
+                     Console.WriteLine(resultAdd); 
+                     */
+                    try
+                    {
+                        FFmpegHelper.init(AnkiMovieData);
+                        string extracted = await FFmpegHelper.ExtractAudio(videoPath, Path.Combine(AnkiMovieData, "extracted.mp3"));
+                        string trimmed = await FFmpegHelper.ExtractAudio(extracted, Path.Combine(AnkiMovieData, "trimmed.mp3"));
+                    }
+                    catch (Xabe.FFmpeg.Exceptions.FFmpegNotFoundException)
+                    { throw new Exception("FFMpeg was not found. Expected output: " + AnkiMovieData); }
+                }
+                else
+                    Console.WriteLine("Anki-senpai didn't notice me...");
             }
-            else
-                Console.WriteLine("Anki-senpai didn't notice me...");
+            catch (Exception x)
+            {
+                Console.WriteLine(x.ToString());
+            }
         }
-
         private static void LoadVideoAndSrt() {
             string path = Path.Combine(AnkiMovieData, "1.json");
             ArtItem ObjectResult = null;
@@ -71,7 +93,6 @@ namespace AnkiMovie_Console_Test {
             videoPath = ObjectResult.MoviePath;
             srtPath = ObjectResult.SrtPath;
         }
-
         private static void PickVideoAndSrt() {
             Console.ResetColor();
             Console.BackgroundColor = ConsoleColor.Black;
@@ -113,7 +134,6 @@ namespace AnkiMovie_Console_Test {
             string json = JsonConvert.SerializeObject(newItem);
             System.IO.File.WriteAllText(Path.Combine(AnkiMovieData, "1.json"), json);
         }
-
         private static List<SubtitleItem> GetSubtitleText(string FilePath) {
             var parser = new SubtitlesParser.Classes.Parsers.SubParser();
             List<SubtitleItem> items = null;
