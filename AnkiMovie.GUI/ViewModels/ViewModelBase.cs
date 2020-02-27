ee511d4886cddf025234560e65c3cd67b4c2caee
Christos <119371@mymerewade.nl>
2020-02-27 21:49:57 +0100

    GUI

---
+++ AnkiMovie.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace AnkiMovie.GUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
